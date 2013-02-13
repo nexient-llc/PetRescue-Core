95f8fe741ae54222379e4621688b9f2f61a25513
skronek <keithskronek@gmail.com>
2013-02-13 08:46:37 +0530

    Added dogBreeds test just to get the list of dog breeds.  Formatting.

--- src/test/java/com/systemsinmotion/petrescue/web/PetFinderConsumerTest.java
+++ src/test/java/com/systemsinmotion/petrescue/web/PetFinderConsumerTest.java
@@ -29,6 +29,7 @@ public class PetFinderConsumerTest {
 
 	private static final String AGE_ADULT = "Adult";
 	private static final String ANIMAL_CAT = "cat";
+	private static final String ANIMAL_DOG = "dog";
 	private static final String BREED_DOMESTIC_SHORT_HAIR = "Domestic Short Hair";
 	private static final String BREED_NULL = null;
 	private static final Integer COUNT_NULL = null;
@@ -47,10 +48,19 @@ public class PetFinderConsumerTest {
 	PetFinderConsumer petFinderService;
 
 	@Test
-	public void breedList() {
-		final PetfinderBreedList breedList = this.petFinderService.breedList(ANIMAL_CAT, null);
+	public void catBreedList() {
+		final PetfinderBreedList breedList = this.petFinderService.breedList(
+				ANIMAL_CAT, null);
 		assertNotNull(breedList);
 		assertTrue(breedList.getAnimal().equals(ANIMAL_CAT));
+	}
+
+	@Test
+	public void dogBreedList() {
+		final PetfinderBreedList breedList = this.petFinderService.breedList(
+				ANIMAL_DOG, null);
+		assertNotNull(breedList);
+		assertTrue(breedList.getAnimal().equals(ANIMAL_DOG));
 	}
 
 	// @Ignore
@@ -60,7 +70,8 @@ public class PetFinderConsumerTest {
 		params.put(QueryParam.animal, ANIMAL_CAT);
 		params.put(QueryParam.breed, BREED_DOMESTIC_SHORT_HAIR);
 		final String token = this.petFinderService.authData().getToken();
-		final String query = this.petFinderService.buildQuery(token, params, true);
+		final String query = this.petFinderService.buildQuery(token, params,
+				true);
 		assertTrue(query.contains("&animal=cat"));
 		assertTrue(query.contains("&breed=Domestic%20Short%20Hair"));
 	}
@@ -68,8 +79,10 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void findPet() {
-		final PetfinderPetRecordList petRecordList = this.petFinderService.findPet(ANIMAL_CAT, BREED_NULL, SIZE_NULL,
-				GENDER_MALE, LOCATION_ZIP, AGE_ADULT, OFFSET_NULL, COUNT_NULL, OUTPUT_FULL, FORMAT_NULL);
+		final PetfinderPetRecordList petRecordList = this.petFinderService
+				.findPet(ANIMAL_CAT, BREED_NULL, SIZE_NULL, GENDER_MALE,
+						LOCATION_ZIP, AGE_ADULT, OFFSET_NULL, COUNT_NULL,
+						OUTPUT_FULL, FORMAT_NULL);
 		assertNotNull(petRecordList);
 		final List<PetfinderPetRecord> pets = petRecordList.getPet();
 		assertNotNull(pets);
@@ -83,8 +96,9 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void findShelter() {
-		final PetfinderShelterRecordList shelterRecordList = this.petFinderService.findShelter(LOCATION_ZIP, NAME_NULL,
-				OFFSET_NULL, COUNT_NULL, FORMAT_NULL);
+		final PetfinderShelterRecordList shelterRecordList = this.petFinderService
+				.findShelter(LOCATION_ZIP, NAME_NULL, OFFSET_NULL, COUNT_NULL,
+						FORMAT_NULL);
 		assertNotNull(shelterRecordList);
 
 	}
@@ -92,13 +106,15 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void getPet() {
-		final PetfinderPetRecord randomPet = this.petFinderService.randomPet(ANIMAL_CAT, BREED_NULL, SIZE_NULL,
-				GENDER_MALE, LOCATION_ZIP, SHELTER_NULL, OUTPUT_FULL, FORMAT_NULL);
+		final PetfinderPetRecord randomPet = this.petFinderService.randomPet(
+				ANIMAL_CAT, BREED_NULL, SIZE_NULL, GENDER_MALE, LOCATION_ZIP,
+				SHELTER_NULL, OUTPUT_FULL, FORMAT_NULL);
 		assertNotNull(randomPet);
 		assertEquals(AnimalType.CAT, randomPet.getAnimal());
 		assertEquals(PetGenderType.M, randomPet.getSex());
 
-		final PetfinderPetRecord pet = this.petFinderService.readPet(randomPet.getId(), null);
+		final PetfinderPetRecord pet = this.petFinderService.readPet(
+				randomPet.getId(), null);
 		assertNotNull(pet);
 		assertEquals(randomPet.getId(), pet.getId());
 	}
@@ -106,7 +122,8 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void getShelter() {
-		final PetfinderShelterRecord shelter = this.petFinderService.readShelter(FORMAT_NULL);
+		final PetfinderShelterRecord shelter = this.petFinderService
+				.readShelter(FORMAT_NULL);
 		assertNotNull(shelter);
 		assertEquals(shelterId, shelter.getId());
 	}
@@ -114,8 +131,9 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void randomPet() {
-		final PetfinderPetRecord randomPet = this.petFinderService.randomPet(ANIMAL_CAT, null, null, GENDER_MALE,
-				LOCATION_ZIP, null, OUTPUT_FULL, null);
+		final PetfinderPetRecord randomPet = this.petFinderService.randomPet(
+				ANIMAL_CAT, null, null, GENDER_MALE, LOCATION_ZIP, null,
+				OUTPUT_FULL, null);
 		assertNotNull(randomPet);
 		assertEquals(AnimalType.CAT, randomPet.getAnimal());
 	}
@@ -123,8 +141,9 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void shelterCats() {
-		final List<PetfinderPetRecord> cats = this.petFinderService.shelterCats(STATUS_NULL, OFFSET_NULL, COUNT_NULL,
-				OUTPUT_FULL, FORMAT_NULL);
+		final List<PetfinderPetRecord> cats = this.petFinderService
+				.shelterCats(STATUS_NULL, OFFSET_NULL, COUNT_NULL, OUTPUT_FULL,
+						FORMAT_NULL);
 		assertNotNull(cats);
 		for (PetfinderPetRecord cat : cats) {
 			assertNotNull(cat);
@@ -135,8 +154,9 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void shelterDogs() {
-		final List<PetfinderPetRecord> dogs = this.petFinderService.shelterDogs(STATUS_NULL, OFFSET_NULL, COUNT_NULL,
-				OUTPUT_FULL, FORMAT_NULL);
+		final List<PetfinderPetRecord> dogs = this.petFinderService
+				.shelterDogs(STATUS_NULL, OFFSET_NULL, COUNT_NULL, OUTPUT_FULL,
+						FORMAT_NULL);
 		assertNotNull(dogs);
 		for (PetfinderPetRecord dog : dogs) {
 			assertNotNull(dog);
@@ -147,8 +167,8 @@ public class PetFinderConsumerTest {
 	// @Ignore
 	@Test
 	public void shelterPets() {
-		List<PetfinderPetRecord> pets = this.petFinderService.shelterPets(STATUS_NULL, OFFSET_NULL, COUNT_NULL,
-				OUTPUT_FULL, FORMAT_NULL);
+		List<PetfinderPetRecord> pets = this.petFinderService.shelterPets(
+				STATUS_NULL, OFFSET_NULL, COUNT_NULL, OUTPUT_FULL, FORMAT_NULL);
 		assertNotNull(pets);
 		PetfinderPetRecord pet = pets.get(0);
 		assertNotNull(pet);
@@ -158,8 +178,9 @@ public class PetFinderConsumerTest {
 	// takes over a minute to return results from PetFinder
 	@Test
 	public void shelterPetsByBreed() {
-		final PetfinderShelterRecordList recordList = this.petFinderService.shelterPetsByBreed(ANIMAL_CAT,
-				BREED_DOMESTIC_SHORT_HAIR, 0, 1, FORMAT_NULL);
+		final PetfinderShelterRecordList recordList = this.petFinderService
+				.shelterPetsByBreed(ANIMAL_CAT, BREED_DOMESTIC_SHORT_HAIR, 0,
+						1, FORMAT_NULL);
 		assertNotNull(recordList);
 	}
 }

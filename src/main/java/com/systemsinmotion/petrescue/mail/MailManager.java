5c7cc888c8a32f69ec424556aaa387edd6bfc05d
skronek <keithskronek@gmail.com>
2013-02-15 01:53:21 +0530

    Now getting email properties from shelter.properties vie Apache Commons Configurer and outputting application values via Googles gson library

--- src/main/java/com/systemsinmotion/petrescue/mail/MailManager.java
+++ src/main/java/com/systemsinmotion/petrescue/mail/MailManager.java
@@ -2,6 +2,7 @@ package com.systemsinmotion.petrescue.mail;
 
 import java.util.Properties;
 
+import javax.annotation.PostConstruct;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -11,9 +12,20 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.systemsinmotion.petrescue.web.bean.AdoptionApplication;
 
+@Component("emailManager")
 public class MailManager {
+	private static final Logger logger = Logger.getLogger(MailManager.class);
+
 	private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
 	private static final String MAIL_SMTP_HOST = "mail.smtp.host";
 	private static final String SUBJECT_FIRST_NAME = "[firstName]";
@@ -27,7 +39,7 @@ public class MailManager {
 	private String username;
 
 	private void addRecipients(MimeMessage message) throws MessagingException, AddressException {
-		for (String recipient : recipients) {
+		for (String recipient : this.recipients) {
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 		}
 	}
@@ -36,11 +48,11 @@ public class MailManager {
 		String subject = this.subject;
 		subject = subject.replace(SUBJECT_FIRST_NAME, application.getFirstName());
 		subject = subject.replace(SUBJECT_LAST_NAME, application.getLastName());
-		subject = subject.replace(SUBJECT_PET_NAME, application.getPet().getName());
+		subject = subject.replace(SUBJECT_PET_NAME, application.getPetName());
 		message.setSubject(subject);
 	}
 
-	private Properties createProperties() {
+	private Properties createMailProperties() {
 		Properties props = System.getProperties();
 		props.put(MAIL_SMTP_HOST, this.host);
 		props.put(MAIL_SMTP_AUTH, "true");
@@ -49,46 +61,45 @@ public class MailManager {
 
 	private Session createSession(Properties props) {
 		return Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+				return new PasswordAuthentication(MailManager.this.username, MailManager.this.password);
 			}
 		});
 	}
 
-	public void send(AdoptionApplication application) throws AddressException, MessagingException {
-		Properties props = createProperties();
+	@PostConstruct
+	public void init() {
+		Configuration config;
+		try {
+			config = new PropertiesConfiguration("shelter.properties");
+			this.host = config.getString("shelter.email.host");
+			this.username = config.getString("shelter.email.username");
+			this.password = config.getString("shelter.email.password");
+			this.from = config.getString("shelter.email.from");
+			this.recipients = config.getString("shelter.email.recipients").split(",");
+			this.subject = config.getString("shelter.email.subject");
+		} catch (ConfigurationException e) {
+			logger.error("File shelter.properties must exist in the classpath.");
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void send(AdoptionApplication application) throws MessagingException {
+		Properties props = createMailProperties();
 		Session session = createSession(props);
 
 		MimeMessage message = new MimeMessage(session);
 		addSubject(message, application);
 		addRecipients(message);
 		message.setFrom(new InternetAddress(this.from));
-		message.setText(application.toString());
+		message.setText(toJson(application));
 
 		Transport.send(message);
 	}
 
-	public void setFrom(String from) {
-		this.from = from;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public void setRecipients(String[] to) {
-		this.recipients = to;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
+	private String toJson(AdoptionApplication application) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.toJson(application);
 	}
 }

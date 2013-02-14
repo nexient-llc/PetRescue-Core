bd0aab7ae91f2d92b6739f5fe7c8dbb99587b000
skronek <keithskronek@gmail.com>
2013-02-15 01:49:09 +0530

    Changed identifier to BigInt since that is what PetFinder API is using and will be more compatible

--- src/main/java/com/systemsinmotion/petrescue/data/repository/PetRecordRepository.java
+++ src/main/java/com/systemsinmotion/petrescue/data/repository/PetRecordRepository.java
@@ -1,11 +1,16 @@
 package com.systemsinmotion.petrescue.data.repository;
 
+import java.math.BigInteger;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.systemsinmotion.petrescue.entity.PetRecord;
 
 @Repository
-public interface PetRecordRepository extends JpaRepository<PetRecord, Long> {
+public interface PetRecordRepository extends JpaRepository<PetRecord, BigInteger> {
+
+	@Override
+	PetRecord findOne(BigInteger petId);
 
 }

23aa5d1ff61a7c29d787185ba7fc5d28fa173b70
Carlos-Mora-SIM <cmora@systemsinmotion.com>
2014-08-19 09:59:59 -0400

    Changed PetService, Pet Repo

--- src/main/java/com/systemsinmotion/petrescue/entity/Photo.java
+++ src/main/java/com/systemsinmotion/petrescue/entity/Photo.java
@@ -1,15 +1,16 @@
 package com.systemsinmotion.petrescue.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-// @Entity
+@Entity
 @Table(name = "photo")
-class Photo extends AbstractPersistable<Long> {
+class Photo extends AbstractPersistable<Integer> {
 
 	private static final long serialVersionUID = 7179570524027044221L;
 
@@ -17,34 +18,33 @@ class Photo extends AbstractPersistable<Long> {
 	@JoinColumn(name = "pet_id")
 	private PetRecord pet;
 
-	@Column(name = "size_type", length = 6, nullable = false)
+	@Column(name = "size", length = 3, nullable = false)
 	private String size;
 
-	@Column(name = "uri", length = 150, nullable = false)
-	private String uri;
+	@Column(name = "url", length = 255, nullable = false)
+	private String url;
 
 	public PetRecord getPet() {
-		return this.pet;
-	}
-
-	public String getSize() {
-		return this.size;
-	}
-
-	public String getUri() {
-		return this.uri;
+		return pet;
 	}
 
 	public void setPet(PetRecord pet) {
 		this.pet = pet;
 	}
 
+	public String getSize() {
+		return size;
+	}
+
 	public void setSize(String size) {
 		this.size = size;
 	}
 
-	public void setUri(String uri) {
-		this.uri = uri;
+	public String getUrl() {
+		return url;
 	}
 
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }

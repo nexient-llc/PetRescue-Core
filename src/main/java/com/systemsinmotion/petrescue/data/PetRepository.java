a18c3db76a0c4813333dba29f8f13ed3e5dba38c
Nathan Thomas <nthomas@systemsinmotion.com>
2014-08-20 09:52:17 -0400

    modified db access

--- src/main/java/com/systemsinmotion/petrescue/data/PetRepository.java
+++ src/main/java/com/systemsinmotion/petrescue/data/PetRepository.java
@@ -3,13 +3,11 @@ package com.systemsinmotion.petrescue.data;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 import com.systemsinmotion.petrescue.entity.PetRecord;
 import com.systemsinmotion.petrescue.entity.type.StatusType;
 
-@NoRepositoryBean
 @Repository("repository")
 public interface PetRepository extends JpaRepository<PetRecord, Integer> {
 

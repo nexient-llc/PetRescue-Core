d94542f11f4a5c18f3e3e75129e86244ded670b3
kskronek <keithskronek@gmail.com>
2015-06-22 12:14:06 -0400

    Importing PropertyPlaceholderConfig and reading property value from there

--- src/main/java/com/systemsinmotion/petrescue/config/PetRescueConfig.java
+++ src/main/java/com/systemsinmotion/petrescue/config/PetRescueConfig.java
@@ -3,26 +3,31 @@ package com.systemsinmotion.petrescue.config;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:/shelter.properties")
+@Import(PropertyPlaceholderConfig.class)
 public class PetRescueConfig {
 
+    @Value("${velocity.runtime.log}")
+    private String velocityLog;
+
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+	public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
 	@Bean
-	public static VelocityEngine velocityEngine() {
+	public VelocityEngine velocityEngine() {
 		VelocityEngine velocityEngine = new VelocityEngine();
 		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
-		velocityEngine.setProperty("class.resource.loader.class", ClasspathResourceLoader.class.getName());
-		velocityEngine.init();
+        velocityEngine.setProperty(RuntimeConstants.RUNTIME_LOG, velocityLog);
+        velocityEngine.init();
 		return velocityEngine;
 	}
 }
